f0e5fbab7d2185f93084576d4af072d47d6aeaaa
Hoai Nam <namst0601@gmail.com>
2022-11-10 21:36:36 +0700

    ZUI

---
+++ happy_new_year.py
@@ -0,0 +1,444 @@
+import turtle
+
+turtle.setup(1000,1000,0,0)
+
+mitul=turtle.Turtle()
+mitul.width(8)
+mitul.color("red")
+new=turtle.getscreen()
+mitul.speed(4)
+
+new.bgcolor("lightblue")
+
+# Hidden Work(penup)
+mitul.left(180)
+mitul.penup()
+mitul.forward(300)
+mitul.right(90)
+mitul.forward(100)
+mitul.pendown()
+
+# Printing H
+
+
+#start to draw
+mitul.forward(50)
+mitul.right(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.left(90)
+
+mitul.penup()
+mitul.forward(50)
+mitul.left(90)
+mitul.pendown()
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.right(90)
+mitul.forward(50)
+
+
+# printing A
+
+mitul.penup()
+mitul.left(90)
+mitul.forward(15)
+mitul.pendown()
+mitul.left(70)
+mitul.forward(110)
+mitul.right(70)
+mitul.right(70)
+mitul.forward(110)
+mitul.left(180)
+mitul.forward(55)
+mitul.left(70)
+mitul.forward(38)
+mitul.left(70)
+mitul.penup()
+mitul.forward(55)
+mitul.left(110)
+
+mitul.forward(100)
+
+# printing P
+
+mitul.left(90)
+mitul.pendown()
+mitul.forward(100)
+mitul.right(90)
+mitul.forward(50)
+mitul.right(20)
+mitul.forward(20)
+mitul.right(70)
+mitul.forward(40)
+mitul.right(70)
+mitul.forward(20)
+mitul.right(20)
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.penup()
+mitul.forward(100)
+
+
+# printing P
+
+mitul.left(90)
+mitul.pendown()
+mitul.forward(100)
+mitul.right(90)
+mitul.forward(50)
+mitul.right(20)
+mitul.forward(20)
+mitul.right(70)
+mitul.forward(40)
+mitul.right(70)
+mitul.forward(20)
+mitul.right(20)
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.penup()
+mitul.forward(100)
+
+# printing Y
+
+mitul.forward(20)
+mitul.pendown()
+mitul.left(90)
+mitul.forward(50)
+mitul.left(30)
+mitul.forward(60)
+mitul.backward(60)
+mitul.right(60)
+mitul.forward(60)
+mitul.backward(60)
+mitul.left(30)
+
+# go to Home
+
+mitul.penup()
+mitul.home()
+
+mitul.color("orange")
+new.bgcolor("lightgreen")
+# setting second row
+
+mitul.backward(300)
+mitul.right(90)
+mitul.forward(60)
+mitul.left(180)
+
+
+# printing B
+
+
+mitul.pendown()
+mitul.forward(100)
+mitul.right(160)
+mitul.forward(100)
+mitul.left(160)
+mitul.forward(100)
+
+
+
+# go to Home
+
+mitul.penup()
+mitul.home()
+
+# setting up
+
+mitul.backward(240)
+mitul.right(90)
+mitul.forward(10)
+mitul.pendown()
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.backward(50)
+mitul.left(90)
+mitul.forward(100)
+mitul.right(90)
+mitul.forward(50)
+mitul.backward(50)
+mitul.right(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.penup()
+mitul.home()
+
+# set up for W
+
+mitul.backward(150)
+mitul.right(90)
+mitul.forward(60)
+mitul.pendown()
+mitul.backward(100)
+mitul.forward(100)
+mitul.left(120)
+mitul.forward(40)
+mitul.right(60)
+mitul.forward(40)
+mitul.left(120)
+mitul.forward(100)
+mitul.penup()
+mitul.home()
+
+# set up for Y
+
+mitul.backward(30)
+mitul.right(90)
+mitul.forward(65)
+mitul.left(90)
+
+
+
+# set up for Y
+
+
+mitul.pendown()
+mitul.left(90)
+mitul.forward(50)
+mitul.left(30)
+mitul.forward(60)
+mitul.backward(60)
+mitul.right(60)
+mitul.forward(60)
+mitul.backward(60)
+mitul.left(30)
+
+# go to Home 
+
+mitul.penup()
+mitul.home()
+
+
+# printing E
+
+mitul.forward(10)
+mitul.right(90)
+mitul.forward(10)
+mitul.pendown()
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.backward(50)
+mitul.left(90)
+mitul.forward(100)
+mitul.right(90)
+mitul.forward(50)
+mitul.backward(50)
+mitul.right(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.forward(50)
+mitul.penup()
+mitul.home()
+
+
+# set up for A
+mitul.forward(90)
+
+# printing A
+
+mitul.right(90)
+mitul.forward(50)
+mitul.left(90)
+mitul.pendown()
+mitul.left(70)
+mitul.forward(110)
+mitul.right(70)
+mitul.right(70)
+mitul.forward(110)
+mitul.left(180)
+mitul.forward(55)
+mitul.left(70)
+mitul.forward(38)
+mitul.left(70)
+mitul.penup()
+mitul.forward(55)
+mitul.left(110)
+
+mitul.forward(100)
+
+
+#go to home
+mitul.penup()
+mitul.home()
+
+
+# set up for R
+mitul.forward(180)
+mitul.right(90)
+mitul.forward(50)
+mitul.left(180)
+
+#set up for R
+mitul.pendown()
+mitul.forward(100)
+mitul.right(90)
+mitul.forward(50)
+mitul.right(20)
+mitul.forward(20)
+mitul.right(70)
+mitul.forward(40)
+mitul.right(70)
+mitul.forward(20)
+mitul.right(20)
+mitul.forward(50)
+mitul.left(180)
+mitul.forward(50)
+mitul.right(20)
+mitul.forward(20)
+mitul.right(70)
+mitul.forward(40)
+mitul.left(180)
+mitul.penup()
+mitul.home()
+
+# setting position
+
+mitul.right(90)
+mitul.forward(215)
+mitul.right(90)
+mitul.forward(170)
+
+
+#color
+
+mitul.color("green")
+new.bgcolor("lightblue")
+
+
+# printing 2
+
+
+mitul.pendown()
+mitul.forward(60)
+mitul.right(90)
+mitul.forward(40)
+mitul.right(90)
+mitul.forward(50)
+mitul.left(20)
+mitul.forward(20)
+mitul.left(70)
+mitul.forward(30)
+mitul.left(20)
+mitul.forward(20)
+mitul.left(70)
+mitul.forward(60)
+
+
+#printing 0
+mitul.penup()
+mitul.home()
+mitul.right(90)
+mitul.forward(170)
+mitul.right(90)
+mitul.forward(50)
+mitul.right(90)
+
+mitul.pendown()
+mitul.circle(40)
+
+# go to homw
+
+mitul.penup()
+mitul.home()
+
+
+# Printing 1
+
+mitul.right(90)
+mitul.forward(215)
+mitul.right(180)
+
+# Set up for 1
+
+mitul.pendown()
+mitul.forward(100)
+mitul.left(90)
+mitul.forward(10)
+mitul.left(20)
+mitul.forward(20)
+
+#go to home
+
+mitul.penup()
+mitul.home()
+
+# setup for 8
+mitul.right(90)
+mitul.forward(160)
+mitul.left(90)
+mitul.forward(70)
+mitul.pendown()
+mitul.circle(35)
+mitul.right(180)
+mitul.circle(35)
+
+#go to home()
+mitul.penup()
+mitul.home()
+
+
+#design pattern
+mitul.home()
+mitul.forward(370)
+mitul.pendown()
+mitul.color("hotpink")
+mitul.width(3)
+mitul.speed(0)
+
+def squre(length, angle):
+    
+    mitul.forward(length)
+    mitul.right(angle)
+    mitul.forward(length)
+    mitul.right(angle)
+   
+    mitul.forward(length)
+    mitul.right(angle)
+    mitul.forward(length)
+    mitul.right(angle)
+
+squre(80, 90)
+
+for i in range(36):
+      mitul.right(10)
+      squre(80, 90)
+
+
+
+mitul.penup()
+mitul.home()
+mitul.left(90)
+mitul.forward(270)
+mitul.left(90)
+mitul.forward(200)
+mitul.pendown()
+
+def draw_star(size, color):
+    angle = 120
+    mitul.fillcolor(color)
+    mitul.begin_fill()
+
+    for side in range(5):
+        mitul.forward(size)
+        mitul.right(angle)
+        mitul.forward(size)
+        mitul.right(72 - angle)
+    mitul.end_fill()
+    return
+
+draw_star(30, "purple")
+
+
+
+turtle.mainloop()
